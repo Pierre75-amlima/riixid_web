8bda4aa097fe5440b1b04085fbb34008f45816b6
Amlima K. Pierre <pierreamlima040@gmail.com>
2026-08-05 17:17:34 +0100

    fix: ScrollToTop au changement de route

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutPage from "./pages/About";
 import ProjectsPage from "./pages/Projects";
+import ScrollToTop from "./components/ScrollToTop";
 import ContactModal from "./components/contact-modal/ContactModal";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/a-propos" element={<AboutPage />} />
